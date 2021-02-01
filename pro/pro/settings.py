ffe65452127e4f2fc21abef83eb69743184de31f
akshaymurari <akshaymurari184@gmail.com>
2021-02-01 11:28:12 +0530

    ..

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=f%d^qq09hk*9c8kstlmx^uz38@_npdjasb2!+ri5)8t3s7lxd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
